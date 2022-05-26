a73a826d850621a86de0db69b592e927d9e6f1ca
Daniel Shrimpton <44175578+DanielShrimpton@users.noreply.github.com>
2022-05-26 13:33:09 +0100

    shanging left encoder tap to soundswitch device change macro

--- keyboards/lotus58/keymaps/custom/keymap.c
+++ keyboards/lotus58/keymaps/custom/keymap.c
@@ -33,6 +33,7 @@ enum custom_keycodes {
     KC_GAME,
     // KC_NUM,
     // KC_FUNC,
+    _SOUND_SWITCH,
 };
 
 // Tap Dance declarations
@@ -85,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     CK_ESC, KC_1, KC_2,   KC_3,     KC_4,    KC_5,                             KC_6, KC_7,      KC_8,    KC_9,   KC_0,    KC_MINS,
     KC_TAB, KC_Q, KC_W,   KC_E,     KC_R,    KC_T,                             KC_Y, KC_U,      KC_I,    KC_O,   KC_P,    KC_LBRC,
    KC_NUHS, KC_A, KC_S,   KC_D,     KC_F,    KC_G,                             KC_H, KC_J,      KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-  CK_LCTRL, KC_Z, KC_X,   KC_C,     KC_V,    KC_B, TD(PG_DN),      TD(PG_UP),  KC_N, KC_M,      KC_COMM, KC_DOT, KC_SLSH, CK_RCTRL,
+  CK_LCTRL, KC_Z, KC_X,   KC_C,     KC_V,    KC_B, _SOUND_SWITCH,      TD(PG_UP),  KC_N, KC_M,      KC_COMM, KC_DOT, KC_SLSH, CK_RCTRL,
             KC_LGUI, CK_DELINS, TT(_NUM), CK_LALT,   CK_LSPC,      CK_RENT, CK_RALT, TT(_FUNC), KC_BSPC, TD(MEDIA_SCROLL)
 ),
 
@@ -220,6 +221,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 set_single_persistent_default_layer(_QWERTY);
             }
             return false;
+
         case KC_GAME:
             if (record->event.pressed) {
                 switch (get_highest_layer(default_layer_state)) {
@@ -232,6 +234,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
             }
             return false;
+        
+        case _SOUND_SWITCH:
+            if (record->event.pressed) {
+                register_code(KC_LCTRL);
+                register_code(KC_LALT);
+                tap_code(KC_F11);
+                unregister_code(KC_LCTRL);
+                unregister_code(KC_LALT);
+            }
     }
     return true;
 }
