cb045349e8c9cc87f2a26099de59d3ab4bd87cf1
Daniel Shrimpton <44175578+DanielShrimpton@users.noreply.github.com>
2022-07-31 14:59:19 +0100

    Optimising key layouts and cleaning up whitespace

--- keyboards/lotus58/keymaps/custom/keymap.c
+++ keyboards/lotus58/keymaps/custom/keymap.c
@@ -82,12 +82,11 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 // Define the layers here
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT(
-
-    CK_ESC, KC_1, KC_2,   KC_3,     KC_4,    KC_5,                             KC_6, KC_7,      KC_8,    KC_9,   KC_0,    KC_MINS,
-    KC_TAB, KC_Q, KC_W,   KC_E,     KC_R,    KC_T,                             KC_Y, KC_U,      KC_I,    KC_O,   KC_P,    KC_LBRC,
-   KC_NUHS, KC_A, KC_S,   KC_D,     KC_F,    KC_G,                             KC_H, KC_J,      KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-  CK_LCTRL, KC_Z, KC_X,   KC_C,     KC_V,    KC_B, _SOUND_SWITCH,      TD(PG_UP),  KC_N, KC_M,      KC_COMM, KC_DOT, KC_SLSH, CK_RCTRL,
-            KC_LGUI, CK_DELINS, TT(_NUM), CK_LALT,   CK_LSPC,      CK_RENT, CK_RALT, TT(_FUNC), KC_BSPC, TD(MEDIA_SCROLL)
+      CK_ESC, KC_1,    KC_2,   KC_3,     KC_4,    KC_5,                              KC_6,    KC_7,      KC_8,    KC_9,   KC_0,    KC_MINS,
+      KC_TAB, KC_Q,    KC_W,   KC_E,     KC_R,    KC_T,                              KC_Y,    KC_U,      KC_I,    KC_O,   KC_P,    KC_LBRC,
+     KC_NUHS, KC_A,    KC_S,   KC_D,     KC_F,    KC_G,                              KC_H,    KC_J,      KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+    KC_LCTRL, KC_Z,    KC_X,   KC_C,     KC_V,    KC_B, _SOUND_SWITCH,    TD(PG_UP), KC_N,    KC_M,      KC_COMM, KC_DOT, KC_SLSH, CK_RCTRL,
+                    XXXXXXX, KC_DEL, TT(_NUM), CK_LALT,       CK_LSPC,    CK_RENT,   CK_RALT, TT(_FUNC), KC_BSPC, XXXXXXX
 ),
 
 // [_NUM] = LAYOUT(
@@ -99,36 +98,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // ),
 
 [_NUM] = LAYOUT(
-  _______,   KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,                         KC_CIRC,    KC_7,    KC_8,    KC_9, XXXXXXX, XXXXXXX,
-  _______, XXXXXXX,   KC_UP,  XXXXXXX, XXXXXXX, XXXXXXX,                         KC_HASH,    KC_4,    KC_5,    KC_6, XXXXXXX, XXXXXXX,
-  XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT, KC_LBRC, KC_RBRC,                         KC_AMPR,    KC_1,    KC_2,    KC_3, XXXXXXX, XXXXXXX,
-  _______, XXXXXXX, KC_PAST,  KC_PPLS, KC_PSLS,  KC_EQL, _______,       _______,  KC_DLR,    KC_0, KC_SCLN, KC_COLN, KC_PMNS, _______,
-                    _______,  _______, _______, _______, _______,       _______, _______, _______, _______, _______
+
+    KC_GRAVE,   KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,                         KC_CIRC, KC_7,    KC_8,    KC_9,    XXXXXXX, XXXXXXX,
+     _______, XXXXXXX,   KC_UP,  XXXXXXX, XXXXXXX, XXXXXXX,                         KC_HASH, KC_4,    KC_5,    KC_6,    XXXXXXX, XXXXXXX,
+     XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT, KC_LBRC, KC_RBRC,                         KC_AMPR, KC_1,    KC_2,    KC_3,    XXXXXXX, KC_NUHS,
+     _______, KC_NUBS, KC_PAST,  KC_PPLS, KC_PSLS,  KC_EQL, _______,       _______, KC_DLR,  KC_0,    KC_SCLN, KC_COLN, KC_PMNS, _______,
+                       XXXXXXX,  _______, _______, _______, _______,       _______, _______, _______, _______, XXXXXXX
 ),
 
 [_FUNC] = LAYOUT(
-  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, _______,
-  _______, _______, _______, _______, _______, _______,                         _______, _______,   KC_UP, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______,                         _______, KC_LEFT, KC_DOWN,KC_RIGHT, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______,       _______, _______, KC_HOME,  KC_END, _______, _______, _______,
-                    _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
+    _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                         KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  _______,
+    _______, _______, _______, _______, _______, _______,                         _______, _______, KC_UP,   _______,  _______, _______,
+    _______, KC_MPLY, KC_MPRV, KC_MNXT, _______, _______,                         _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, KC_NUHS,
+    _______, _______, _______, _______, _______, _______, _______,       _______, _______, KC_HOME, KC_END,  _______,  _______, _______,
+                      XXXXXXX, _______, _______, KC_LGUI, _______,       _______, _______, _______, _______, XXXXXXX
 ),
 
-  [_SYSTEM] = LAYOUT(
-  XXXXXXX , XXXXXXX,  XXXXXXX ,  XXXXXXX , XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  RESET  , XXXXXXX,KC_QWERTY,KC_GAME,XXXXXXX,KC_ASTG,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX , XXXXXXX,XXXXXXX, XXXXXXX,    XXXXXXX,  XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,  XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                   _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______ 
+[_SYSTEM] = LAYOUT(
+    XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      RESET, XXXXXXX, KC_QWERTY, KC_GAME, XXXXXXX, KC_ASTG,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                        XXXXXXX, _______, _______, _______, _______,     _______, _______, _______, _______, XXXXXXX 
 ),
 
 [_GAME] = LAYOUT(
-
-    KC_ESC, KC_1, KC_2,   KC_3,     KC_4,    KC_5,                             KC_6, KC_7,      KC_8,    KC_9,   KC_0,    KC_MINS,
-    KC_TAB, KC_Q, KC_W,   KC_E,     KC_R,    KC_T,                             KC_Y, KC_U,      KC_I,    KC_O,   KC_P,    KC_LBRC,
-   KC_CAPS, KC_A, KC_S,   KC_D,     KC_F,    KC_G,                             KC_H, KC_J,      KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-   KC_LSFT, KC_Z, KC_X,   KC_C,     KC_V,    KC_B, TD(PG_DN),      TD(PG_UP),  KC_N, KC_M,      KC_COMM, KC_DOT, KC_SLSH, CK_RCTRL,
-            KC_LGUI, TT(_NUM),   KC_LCTL, KC_LALT, KC_SPC,      CK_RENT, CK_RALT, TT(_FUNC), KC_BSPC, TD(MEDIA_SCROLL)
+     KC_ESC,  KC_1,    KC_2,      KC_3,     KC_4,    KC_5,                             KC_6,    KC_7,      KC_8,      KC_9,    KC_0,    KC_MINS,
+     KC_TAB,  KC_Q,    KC_W,      KC_E,     KC_R,    KC_T,                             KC_Y,    KC_U,      KC_I,      KC_O,    KC_P,    KC_LBRC,
+    KC_CAPS,  KC_A,    KC_S,      KC_D,     KC_F,    KC_G,                             KC_H,    KC_J,      KC_K,      KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT,  KC_Z,    KC_X,      KC_C,     KC_V,    KC_B, TD(PG_DN),      TD(PG_UP),  KC_N,    KC_M,      KC_COMM,   KC_DOT,  KC_SLSH, CK_RCTRL,
+                   XXXXXXX,   TT(_NUM),  KC_LCTL, KC_LALT,    KC_SPC,      CK_RENT,    CK_RALT, TT(_FUNC), KC_BSPC,   XXXXXXX
 ),
 };
 
